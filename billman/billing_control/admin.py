620a79dac6db96f90d5065bfc9c394560774c4b8
ftbarata <fellipetheophilo@gmail.com>
2017-08-18 23:01:55 -0300

    Admin register included billing history model

--- billman/billing_control/admin.py
+++ billman/billing_control/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import BillingControl, ScheduledPrice
+from .models import BillingControl, ScheduledPrice, BillingHistory
 
 admin.site.register(BillingControl)
 admin.site.register(ScheduledPrice)
+admin.site.register(BillingHistory)
